cb57f6c069cea5af2c87f5ce95a35d6a206aa950
David <davidalaga2@gmail.com>
2025-02-08 23:54:03 -0800

    Updated code for feature XYZ

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 document.getElementById("whatsappBtn").addEventListener("click", function() {
     window.location.href = "https://wa.me/+2347083429887";
 });
+
+document.getElementById("groupBtn").addEventListener("click", function() {
+    window.location.href = "https://chat.whatsapp.com/CLrwUgHd4YJ5GlEx76svIB";
+});
